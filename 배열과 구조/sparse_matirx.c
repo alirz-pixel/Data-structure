dd88c3678e9f48b3efb994629b7ba04e8d3db2fb
alirz-pixel <alirce@naver.com>
2021-07-06 16:45:56 +0900

    Sparse - Add addMatrix() function

--- 배열과 구조/sparse_matirx.c
+++ 배열과 구조/sparse_matirx.c
@@ -14,6 +14,7 @@ test case
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_MATRIX 100
 
 typedef struct {
@@ -30,6 +31,8 @@ void waring();
 void printMenu(int* menu);
 void printMatrix();
 void Transpose();
+int  findMax(int a, int b);
+void addMatrix();
 
 int main(void)
 {   
@@ -59,11 +62,13 @@ int main(void)
 
         
         else if (menu == 2)
-            printMatrix();
+            printMatrix(sparseMatrix);
 
         else if (menu == 3)
             Transpose();
 
+        else if (menu == 4)
+            addMatrix();
     } while(menu);
 
     return 0;
@@ -84,16 +89,17 @@ void printMenu(int* menu)
     printf("1. 희소행렬 입력하기\n");
     printf("2. 희소행렬 출력하기\n");
     printf("3. 전치 행렬 구하기\n");
+    printf("4. 두 희소행렬 더하기\n");
     printf("========\n\n");
 
     printf("입력 : ");
     scanf("%d", menu);
 }
 
-void printMatrix()
+void printMatrix(Matrix matrix[])
 {
-    for (int i = 0; i <= sparseMatrix[0].value; i++)
-        printf("%d %d %d\n", sparseMatrix[i].row, sparseMatrix[i].col, sparseMatrix[i].value);
+    for (int i = 0; i <= matrix[0].value; i++)
+        printf("%d %d %d\n", matrix[i].row, matrix[i].col, matrix[i].value);
 }
 
 void Transpose()
@@ -117,4 +123,90 @@ void Transpose()
 
     for (int i = 0; i <= trans_Matrix[0].value; i++)
         printf("%d %d %d\n", trans_Matrix[i].row, trans_Matrix[i].col, trans_Matrix[i].value);
+}
+
+int findMax(int a, int b)
+{
+    if (a < b)
+        return b;
+
+    else
+        return a;
+}
+
+void addMatrix()
+{
+    int aIndex = 1, bIndex = 1, tempIndex = 1;
+
+    Matrix* add_Matrix = malloc(sizeof(Matrix) * MAX_MATRIX);
+
+    add_Matrix[0].value = 0;
+    add_Matrix[0].row = findMax(sparseMatrix[0].row, trans_Matrix[0].row);
+    add_Matrix[0].col = findMax(sparseMatrix[0].col, trans_Matrix[0].col);
+    while(aIndex <= sparseMatrix[0].value && bIndex <= trans_Matrix[0].value)
+    {
+        if (sparseMatrix[aIndex].row == trans_Matrix[bIndex].row)
+        {
+            add_Matrix[tempIndex].row = sparseMatrix[aIndex].row;
+
+            if (sparseMatrix[aIndex].col == trans_Matrix[bIndex].col)
+            {
+                add_Matrix[tempIndex].col = sparseMatrix[aIndex].col;
+                add_Matrix[tempIndex++].value = sparseMatrix[aIndex++].value + trans_Matrix[bIndex++].value;
+            }
+
+            else if (sparseMatrix[aIndex].col < trans_Matrix[bIndex].col)
+            {
+                add_Matrix[tempIndex].col = sparseMatrix[aIndex].col;
+                add_Matrix[tempIndex++].value = sparseMatrix[aIndex++].value;
+            }
+
+            else
+            {
+                add_Matrix[tempIndex].col = trans_Matrix[bIndex].col;
+                add_Matrix[tempIndex++].value = trans_Matrix[bIndex++].value;
+            }
+        }
+
+        else if (sparseMatrix[aIndex].row < trans_Matrix[bIndex].row)
+        {
+            add_Matrix[tempIndex].row = sparseMatrix[aIndex].row;
+            add_Matrix[tempIndex].col = sparseMatrix[aIndex].col;
+            add_Matrix[tempIndex++].value = sparseMatrix[aIndex++].value;
+        }
+
+        else
+        {
+            add_Matrix[tempIndex].row = trans_Matrix[bIndex].row;
+            add_Matrix[tempIndex].col = trans_Matrix[bIndex].col;
+            add_Matrix[tempIndex++].value = trans_Matrix[bIndex++].value;
+        }
+
+        add_Matrix[0].value++;
+    }
+
+    // aIndex <= sparseMatrix[0].value && bIndex <= trans_Matrix[0].value;
+    if (aIndex > sparseMatrix[0].value)
+        while(bIndex <= trans_Matrix[0].value)
+        {
+            add_Matrix[tempIndex].row = trans_Matrix[bIndex].row;
+            add_Matrix[tempIndex].col = trans_Matrix[bIndex].col;
+            add_Matrix[tempIndex++].value = trans_Matrix[bIndex++].value;
+
+            add_Matrix[0].value++;
+        }
+
+    else
+        while(aIndex <= sparseMatrix[0].value)
+        {
+            add_Matrix[tempIndex].row = sparseMatrix[aIndex].row;
+            add_Matrix[tempIndex].col = sparseMatrix[aIndex].col;
+            add_Matrix[tempIndex++].value = sparseMatrix[aIndex++].value;
+
+            add_Matrix[0].value++;
+        }
+
+
+    printMatrix(add_Matrix);
+    free(add_Matrix);
 }
